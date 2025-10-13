4f7e2da9bf9bddc28da92b47f2f917141aba506c
Chen Wang <uranuswch@gmail.com>
2025-10-13 19:42:32 +0800

    feat(common): add config manager
    Introduce a new flexible configuration management library for Go, which supports multiple data providers including PostgreSQL and Redis. This library also enables automatic configuration reloading.
    
    - Added `configmanager` which supports PostgreSQL and Redis with auto-reload.
    - Included tests and documentation for the `configmanager`.
    - Updated MODULE.bazel with new dependencies and package configurations.
    - Created a dummy service with Bazel build rules for testing integrations.
    
    These changes enhance the project's configurability and integration capabilities with external systems, improving its robustness and flexibility in diverse environments.

---
+++ common/configmanager/manager.go
@@ -0,0 +1,192 @@
+package configmanager
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"sentioxyz/sentio-core/common/log"
+
+	"github.com/knadh/koanf/v2"
+	"github.com/samber/lo"
+)
+
+var ErrReloadNotSupported = fmt.Errorf("provider not support reload")
+
+type LoadParams struct {
+	EnableReload bool
+	ReloadPeriod time.Duration
+	ReloadFunc   func(src, desc map[string]any) error
+	StrictMode   bool
+	Delim        string
+}
+
+type Manager interface {
+	Get(name string) Config
+	Load(name string, provider koanf.Provider, parser koanf.Parser, params *LoadParams) error
+	Shutdown() error
+}
+
+type Config interface {
+	String(path string) string
+	MustString(path string) string
+	Strings(path string) []string
+	MustStrings(path string) []string
+	StringMap(path string) map[string]string
+	MustStringMap(path string) map[string]string
+	Bytes(path string) []byte
+	MustBytes(path string) []byte
+
+	Bool(path string) bool
+	Bools(path string) []bool
+	MustBools(path string) []bool
+	BoolMap(path string) map[string]bool
+	MustBoolMap(path string) map[string]bool
+
+	Int64(path string) int64
+	MustInt64(path string) int64
+	Int64s(path string) []int64
+	MustInt64s(path string) []int64
+	Int64Map(path string) map[string]int64
+	MustInt64Map(path string) map[string]int64
+
+	Int(path string) int
+	MustInt(path string) int
+	Ints(path string) []int
+	MustInts(path string) []int
+	IntMap(path string) map[string]int
+	MustIntMap(path string) map[string]int
+
+	Float64(path string) float64
+	MustFloat64(path string) float64
+	Float64s(path string) []float64
+	MustFloat64s(path string) []float64
+	Float64Map(path string) map[string]float64
+	MustFloat64Map(path string) map[string]float64
+
+	Duration(path string) time.Duration
+	MustDuration(path string) time.Duration
+	Time(path, layout string) time.Time
+	MustTime(path, layout string) time.Time
+
+	LoadAt() time.Time
+}
+
+type ExtendedProvider interface {
+	Watch(period time.Duration, cb func(body any, err error))
+	Unwatch()
+}
+
+type config struct {
+	*koanf.Koanf
+	name     string
+	provider koanf.Provider
+	parser   koanf.Parser
+	params   *LoadParams
+	loadAt   time.Time
+}
+
+func (c *config) LoadAt() time.Time {
+	return c.loadAt
+}
+
+var (
+	managerOnce   sync.Once
+	globalManager Manager
+)
+
+type manager struct {
+	configs map[string]*config
+	mutex   sync.RWMutex
+}
+
+func (m *manager) Get(name string) Config {
+	m.mutex.RLock()
+	defer m.mutex.RUnlock()
+	return m.configs[name]
+}
+
+func (m *manager) Load(name string, provider koanf.Provider, parser koanf.Parser, params *LoadParams) error {
+	m.mutex.RLock()
+	if _, ok := m.configs[name]; ok {
+		m.mutex.RUnlock()
+		return fmt.Errorf("config %s already loaded", name)
+	}
+	m.mutex.RUnlock()
+
+	k := koanf.NewWithConf(koanf.Conf{
+		Delim:       lo.If(params.Delim == "", ".").Else(params.Delim),
+		StrictMerge: params.StrictMode,
+	})
+	var options []koanf.Option
+	if params.ReloadFunc != nil {
+		options = append(options, koanf.WithMergeFunc(params.ReloadFunc))
+	}
+	if err := k.Load(provider, parser, options...); err != nil {
+		return err
+	}
+	c := &config{
+		Koanf:    k,
+		name:     name,
+		provider: provider,
+		parser:   parser,
+		params:   params,
+		loadAt:   time.Now(),
+	}
+	if c.params.EnableReload {
+		ep, ok := provider.(ExtendedProvider)
+		if !ok {
+			return ErrReloadNotSupported
+		}
+		ep.Watch(c.params.ReloadPeriod, func(body any, err error) {
+			if err != nil {
+				log.Warnf("watch config %s error: %v", name, err)
+				return
+			}
+
+			log.With("body", body).Infof("config %s changed, Reloading...", name)
+			if err := c.Koanf.Load(c.provider, c.parser, options...); err != nil {
+				log.Errorf("reload config %s error: %v", name, err)
+			}
+			c.loadAt = time.Now()
+		})
+	}
+
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+	m.configs[name] = c
+	return nil
+}
+
+func (m *manager) Shutdown() error {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+	for _, c := range m.configs {
+		ep, ok := c.provider.(ExtendedProvider)
+		if ok {
+			ep.Unwatch()
+		}
+	}
+	m.configs = make(map[string]*config)
+	return nil
+}
+
+func init() {
+	managerOnce.Do(func() {
+		globalManager = &manager{
+			configs: make(map[string]*config),
+		}
+	})
+}
+
+func Get(name string) Config {
+	return globalManager.Get(name)
+}
+
+func Load(name string, provider koanf.Provider, parser koanf.Parser, params *LoadParams) error {
+	return globalManager.Load(name, provider, parser, params)
+}
+
+func Shutdown() error {
+	return globalManager.Shutdown()
+}
---
+++ common/configmanager/manager_test.go
@@ -0,0 +1,134 @@
+package configmanager
+
+import (
+	"context"
+	"math/rand"
+	"strconv"
+	"testing"
+	"time"
+
+	"sentioxyz/sentio-core/common/db"
+
+	"github.com/alicebob/miniredis/v2"
+	embeddedpostgres "github.com/fergusstrange/embedded-postgres"
+	"github.com/knadh/koanf/parsers/json"
+	"github.com/redis/go-redis/v9"
+	"github.com/stretchr/testify/suite"
+	"gorm.io/gorm"
+)
+
+type ManagerSuite struct {
+	suite.Suite
+	pgPort    int
+	pg        *embeddedpostgres.EmbeddedPostgres
+	redisPort string
+	db        *gorm.DB
+	redis     *redis.Client
+}
+
+func (s *ManagerSuite) SetupSuite() {
+	s.pgPort = rand.Intn(1000) + 5000
+	s.pg = embeddedpostgres.NewDatabase(embeddedpostgres.DefaultConfig().
+		Username("default").
+		Password("password").
+		Database("default").
+		Port(uint32(s.pgPort)))
+	err := s.pg.Start()
+	if err != nil {
+		panic(err)
+	}
+	r, err := miniredis.Run()
+	if err != nil {
+		panic(err)
+	}
+	s.redisPort = r.Port()
+
+	s.db = db.ConnectDB("postgres://default:password@localhost:" + strconv.FormatInt(int64(s.pgPort), 10) + "/default?sslmode=disable")
+	if err := s.db.AutoMigrate(&SentioConfig{}); err != nil {
+		panic(err)
+	}
+	for _, c := range pgInitializedConfig {
+		_ = s.db.Create(&c)
+	}
+	s.redis = redis.NewClient(&redis.Options{
+		Addr:     "localhost:" + s.redisPort,
+		Password: "",
+		DB:       0,
+	})
+	s.redis.Set(context.Background(), "redis_test", `{"key": "value"}`, 0)
+}
+
+func (s *ManagerSuite) TearDownSuite() {
+	_ = s.pg.Stop()
+	_ = s.redis.Close()
+}
+
+func (s *ManagerSuite) SetupTest() {
+	_ = Shutdown()
+}
+
+func Test_ManagerSuite(t *testing.T) {
+	suite.Run(t, new(ManagerSuite))
+}
+
+func (s *ManagerSuite) Test_Init() {
+	c := Get("config1")
+	s.Nil(c)
+}
+
+func (s *ManagerSuite) Test_Set() {
+	s.Nil(Load("config1", NewPgProvider(s.db, WithPgKey("test_1")), json.Parser(), &LoadParams{}))
+	s.Nil(Load("config2", NewPgProvider(s.db, WithPgKey("test_3")), json.Parser(), &LoadParams{}))
+	s.NotNil(Load("config2", NewPgProvider(s.db, WithPgKey("redis_test")), json.Parser(), &LoadParams{}))
+}
+
+func (s *ManagerSuite) Test_Get() {
+	s.Nil(Load("config1", NewPgProvider(s.db, WithPgKey("test_1")), json.Parser(), &LoadParams{}))
+	c := Get("config1")
+	s.NotNil(c)
+	s.EqualValues(1, c.Int64("num_1"))
+	s.EqualValues(2, c.MustInt64("num_2"))
+
+	s.Nil(Load("config2", NewPgProvider(s.db, WithPgKey("test_3")), json.Parser(), &LoadParams{}))
+	c = Get("config2")
+	s.NotNil(c)
+	s.EqualValues("a", c.String("str_1"))
+
+	s.Nil(Load("r_config", NewRedisProvider(s.redis, WithRedisKey("redis_test")), json.Parser(), &LoadParams{}))
+	c = Get("r_config")
+	s.NotNil(c)
+	s.EqualValues("value", c.String("key"))
+}
+
+func (s *ManagerSuite) Test_Refresh() {
+	s.Nil(Load("config1", NewPgProvider(s.db, WithPgKey("test_1")), json.Parser(), &LoadParams{
+		EnableReload: true,
+		ReloadPeriod: time.Second,
+	}))
+
+	s.db.Model(&SentioConfig{}).Where("key = ?", "test_1").Update("data", `{"num_1": 3}`)
+	time.Sleep(time.Second * 3)
+	c := Get("config1")
+	s.NotNil(c)
+	s.EqualValues(3, c.Int64("num_1"))
+
+	s.db.Model(&SentioConfig{}).Where("key = ?", "test_1").Update("data", `{"num_1": 4}`)
+	time.Sleep(time.Second * 3)
+	c = Get("config1")
+	s.NotNil(c)
+	s.EqualValues(4, c.Int64("num_1"))
+
+	s.Nil(Load("r_config", NewRedisProvider(s.redis, WithRedisKey("redis_test")), json.Parser(), &LoadParams{
+		EnableReload: true,
+		ReloadPeriod: time.Second,
+	}))
+	c = Get("r_config")
+	s.NotNil(c)
+	s.EqualValues("value", c.String("key"))
+
+	s.redis.Set(context.Background(), "redis_test", `{"key": "value2"}`, 0)
+	time.Sleep(time.Second * 3)
+	c = Get("r_config")
+	s.NotNil(c)
+	s.EqualValues("value2", c.String("key"))
+}

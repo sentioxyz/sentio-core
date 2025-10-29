b95bd549253387bf899fbe93ae14d03ae5c4c4d3
Yulong <allendragon@gmail.com>
2025-10-29 16:23:37 +0800

    chore(repo): migrate service storage system interface

---
+++ service/common/storagesystem/storage_settings.go
@@ -0,0 +1,18 @@
+package storagesystem
+
+type StorageSettings struct {
+	GcsServiceAccount  string
+	Bucket             string
+	Prefix             string
+	UploadPrefix       string
+	S3Endpoint         string
+	S3AccessKeyID      string
+	S3SecretKey        string
+	S3InternalEndpoint string
+	DefaultStorage     string
+	CacheStorage       string
+	WalrusAggregators  []string
+	WalrusPublishers   []string
+	WalrusEpochs       int
+	WalrusJWTSecret    string
+}

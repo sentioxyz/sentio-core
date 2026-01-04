7a31b92363b6c3fe4c5a7357b86001a4de89fdfb
Chen Wang <uranuswch@gmail.com>
2026-01-04 21:43:35 +0800

    feat(common): add support for sharding by role and category
    Introduce new sharding functionality that supports
    credential lookup based on a combination of role
    and category. Added `roles.go` to define default roles
    and categories. Modified `sharding.go` with the method
    `shardingCredentialsKey` to generate keys for credential
    lookup. Enhanced `ShardingParameter` with a `Category`
    field and included helper functions `WithCategory` and
    `NewShardingParameter` to simplify parameter initialization.
    This enhancement allows more granular control and reduces
    the likelihood of credential mismatches.

---
+++ common/clickhousemanager/roles.go
@@ -0,0 +1,18 @@
+package ckhmanager
+
+const (
+	DefaultRole      = "default_viewer"
+	SmallEngineRole  = "small_viewer"
+	MediumEngineRole = "medium_viewer"
+	LargeEngineRole  = "large_viewer"
+	UltraEngineRole  = "ultra_viewer"
+
+	EmptyRole = "viewer"
+)
+
+const (
+	SentioCategory   = "sentio"
+	SubgraphCategory = "subgraph"
+
+	DefaultCategory = SubgraphCategory
+)

86ceee63997b82f2e41f5e52ac3331eda06dff3d
Chen Wang <uranuswch@gmail.com>
2026-01-23 11:47:27 +0800

    feat(network): add state management with Postgres and file storage support
    Introduce state management for the network package by adding a new Bazel
    build file and implementing key state components. Define interfaces
    for State and add PlainState to maintain state in memory. Implement
    StateMirrored to provide mirrored state updates and synchronization
    capabilities. Create FileStore for YAML-based state persistence and
    PostgresStore for database-backed state storage using GORM. This
    provides flexible state storage options and prepares the codebase for
    more scalable solutions with the mirrored state management feature.

---
+++ network/state/types.go
@@ -0,0 +1,14 @@
+package state
+
+type IndexerInfo struct {
+	IndexerId           uint64 `json:"indexerId" yaml:"indexer_id"`
+	IndexerUrl          string `json:"indexerUrl" yaml:"indexer_url"`
+	ComputeNodeRpcPort  uint16 `json:"computeNodeRpcPort" yaml:"compute_node_rpc_port"`
+	StorageNodeRpcPort  uint16 `json:"storageNodeRpcPort" yaml:"storage_node_rpc_port"`
+	ClickhouseProxyPort uint16 `json:"clickhouseProxyPort" yaml:"clickhouse_proxy_port"`
+}
+
+type ProcessorAllocation struct {
+	ProcessorId string `json:"processorId" yaml:"processor_id"`
+	IndexerId   uint64 `json:"indexerId" yaml:"indexer_id"`
+}

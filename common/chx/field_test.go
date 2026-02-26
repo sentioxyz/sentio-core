a95cbca3713fed94321ff4ee42ee01dd7f736b74
jupiterv2 <jupiterv2.10@gmail.com>
2026-02-26 18:00:49 +0800

    fix(chx): build field type for decimal has bug

--- common/chx/field_test.go
+++ common/chx/field_test.go
@@ -18,6 +18,7 @@ func Test_BuildFieldType(t *testing.T) {
 	assert.Equal(t, FieldTypeUInt256, BuildFieldType("UInt256"))
 	assert.Equal(t, FieldTypeFloat32, BuildFieldType("Float32"))
 	assert.Equal(t, FieldTypeFloat64, BuildFieldType("Float64"))
+	assert.Equal(t, FieldTypeDecimal{Precision: 76, Scale: 30}, BuildFieldType("Decimal(76, 30)"))
 	assert.Equal(t, FieldTypeEnum{"aaa", "bbb"}, BuildFieldType("Enum8('aaa' = 1, 'bbb' = 2)"))
 	assert.Equal(t, FieldTypeEnum{"aaa", "bbb"}, BuildFieldType("Enum('aaa', 'bbb')"))
 	assert.Equal(t, FieldTypeDateTime64{Precision: 6, Timezone: "UTC"}, BuildFieldType("DateTime64(6, 'UTC')"))
@@ -30,6 +31,39 @@ func Test_BuildFieldType(t *testing.T) {
 			},
 		},
 	}, BuildFieldType("Array(Array(Nullable(Enum8('aaa' = 1, 'bbb' = 2))))"))
+}
+
+func Test_SameAs(t *testing.T) {
+	{
+		a := BuildFieldType("Enum8('AAA' = 1, 'BBB' = 2, 'CCC' = 3)")
+		b := BuildFieldType("Enum('AAA', 'BBB', 'CCC')")
+		assert.True(t, a.SameAs(b))
+	}
+	{
+		a := BuildFieldType("Enum8('AAA' = 1, 'BBB' = 2, 'CCC' = 3)")
+		b := BuildFieldType("Enum('AAA', 'BBB', 'CCC', 'DDD')")
+		assert.False(t, a.SameAs(b))
+	}
+	{
+		a := BuildFieldType("Array(Nullable(Enum8('AAA' = 1, 'BBB' = 2, 'CCC' = 3)))")
+		b := BuildFieldType("Array(Nullable(Enum('AAA', 'BBB', 'CCC')))")
+		assert.True(t, a.SameAs(b))
+	}
+	{
+		a := BuildFieldType("Array(Nullable(Enum8('AAA' = 1, 'BBB' = 2, 'CCC' = 3)))")
+		b := BuildFieldType("Array(Nullable(Enum('AAA', 'BBB', 'CCC', 'DDD')))")
+		assert.False(t, a.SameAs(b))
+	}
+	{
+		a := BuildFieldType("Array(Array(Decimal(76, 30)))")
+		b := FieldTypeArray{Inner: FieldTypeArray{Inner: FieldTypeDecimal{Precision: 76, Scale: 30}}}
+		assert.True(t, a.SameAs(b))
+	}
+	{
+		a := BuildFieldType("Array(Array(Decimal(76, 20)))")
+		b := FieldTypeArray{Inner: FieldTypeArray{Inner: FieldTypeDecimal{Precision: 76, Scale: 30}}}
+		assert.False(t, a.SameAs(b))
+	}
 }
 
 func Test_CheckModify(t *testing.T) {

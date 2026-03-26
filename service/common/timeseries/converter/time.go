be8c396c8dfe57f4ed48eb5cbe739ecb0327be8e
poetry <chen@sentio.xyz>
2026-03-26 11:52:13 +0800

    fix(timeseries): init time range from existing matrix data
    When a matrix already has data, initialize the time bounds from
    the actual data range rather than fixed sentinel values, avoiding
    redundant range expansion on non-empty matrices.

--- service/common/timeseries/converter/time.go
+++ service/common/timeseries/converter/time.go
@@ -5,6 +5,9 @@ import (
 	"time"
 
 	"sentioxyz/sentio-core/service/common/timerange"
+	"sentioxyz/sentio-core/service/common/timeseries/matrix"
+
+	"github.com/samber/lo"
 )
 
 type Time struct {
@@ -13,11 +16,16 @@ type Time struct {
 	reqEndTime   time.Time
 }
 
-func newTime(timeRange *timerange.TimeRange) *Time {
+func newTime(timeRange *timerange.TimeRange, matrix matrix.Matrix) *Time {
+	var (
+		year  = time.Hour * 24 * 365
+		start = lo.If(matrix != nil && matrix.Len() != 0, time.Now().Add(year*10)).Else(timeRange.Start.Add(timeRange.Step * -1))
+		end   = lo.If(matrix != nil && matrix.Len() != 0, time.Unix(0, 0)).Else(timeRange.End.Add(timeRange.Step))
+	)
 	return &Time{
 		TimeRange: &timerange.TimeRange{
-			Start:    time.Now().Add(time.Hour * 24 * 365),
-			End:      time.Unix(0, 0),
+			Start:    start,
+			End:      end,
 			Step:     timeRange.Step,
 			Timezone: timeRange.Timezone,
 		},
